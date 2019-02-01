f6cd65dab09ce12b35c2c9504a2d3a078180c4cd
shinot <shinot8324@gmail.com>
2019-02-01 13:10:01 +0900

    :wrench:Prepare Typescript test code execution environment.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,9 +4,11 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
+    "^.+\\.tsx?$": "ts-jest",
     '.*\\.(vue)$': 'vue-jest',
-  }
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)$"
 }
